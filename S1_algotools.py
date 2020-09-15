4d5ae7eb96177bd3f8da6af509deefc750ac959c
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2020-09-15 21:41:29 +0200

    Update S1_algotools.py

--- S1_algotools.py
+++ S1_algotools.py
@@ -3,6 +3,8 @@ a=1
 b='e'+a
 '''
 
+'''//////////////////////////////////////////////////'''
+
 def average_above_zero(table:list):
  '''
  This function return the average of a table given in parameters
@@ -20,8 +22,7 @@ def average_above_zero(table:list):
  return som/n
  
   
-maTable = [1,2,3,4,5,10]
-print(average_above_zero(maTable))
+'''//////////////////////////////////////////////////'''
 
 
 def max_value(table:list):
@@ -38,24 +39,28 @@ def max_value(table:list):
              vmax = (table[i])
  return vmax
  
-  
-maTable = [1,2,3,4,5,10]
-print(max_value(maTable))
+'''//////////////////////////////////////////////////'''
 
 def reverse_table(table:list):
  '''
  This function return the reverese form of a table given in parameters
  Args : 
      table : the list        
- Returns : the max value of the list
+ Returns :  value of the list
  '''
- trie = false
- for i in range(len(table) - 1):
-     print("test")
-     if table[i] < table[i + 1]:
-         table[i] = table[i + 1]
+ i = 0
+ j = len(table) - 1
+ for i in range(int(j/2)):
+     permut = table[i]
+     table[i] = table[j]
+     table[j] = permut
+     i = i+1
+     j= j-1
  return table
  
   
-maTable = [1,2,3,4,5,10]
+
+maTable = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+'''print(max_value(maTable))'''
+'''print(average_above_zero(maTable))'''
 print(reverse_table(maTable))
