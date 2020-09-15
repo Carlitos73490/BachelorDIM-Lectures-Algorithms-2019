6a876f3e92a25fed0d80acdc7a44b3ddfb5f4598
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2020-09-15 17:37:14 +0200

    Add files via upload

--- S1_algotools.py
+++ S1_algotools.py
@@ -22,3 +22,40 @@ def average_above_zero(table:list):
   
 maTable = [1,2,3,4,5,10]
 print(average_above_zero(maTable))
+
+
+def max_value(table:list):
+ '''
+ This function return the max of a table given in parameters
+ Args : 
+     table : the list        
+ Returns : the max value of the list
+ '''
+ vmax = 0
+ for i in range(len(table)):
+     if len(table) > 0 :
+         if vmax < (table[i]):
+             vmax = (table[i])
+ return vmax
+ 
+  
+maTable = [1,2,3,4,5,10]
+print(max_value(maTable))
+
+def reverse_table(table:list):
+ '''
+ This function return the reverese form of a table given in parameters
+ Args : 
+     table : the list        
+ Returns : the max value of the list
+ '''
+ trie = false
+ for i in range(len(table) - 1):
+     print("test")
+     if table[i] < table[i + 1]:
+         table[i] = table[i + 1]
+ return table
+ 
+  
+maTable = [1,2,3,4,5,10]
+print(reverse_table(maTable))
