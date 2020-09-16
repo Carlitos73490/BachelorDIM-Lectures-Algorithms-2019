fb2290a2cd122cbe1f3bda81f668a256e21c80b3
Carl FUCHS <fuchsca@iut-acy.local>
2020-09-16 10:55:15 +0200

    bouding box four loop nearly fully working

--- S1_algotools.py
+++ S1_algotools.py
@@ -57,10 +57,97 @@ def reverse_table(table:list):
      i = i+1
      j= j-1
  return table
+
+'''//////////////////////////////////////////////////'''
+import numpy as np
+def roi_bbox(input_image: np.array):
+ '''
+ This function return the reverese form of a table given in parameters
+ Args : 
+     table : the list        
+ Returns   
+ ''' 
+ stop = 0
+ '''coin haut gauche'''
+ for i in range(len(input_image)):
+  for j in range(len(input_image[0])):
+      if input_image[i,j] > 0:
+       CHG = i + 1, j+ 1
+       stop = 1
+       break
+  if stop == 1:
+   break
+
+ stop = 0
+   
+ print("///////////////////")
+ 
+ '''coin haut droit'''
+ for i in range(len(input_image)):
+  for j in reversed(range(len(input_image[0]))):
+      if input_image[i,j] > 0:
+       CHD = i + 1, j+ 1
+       stop = 1
+       break
+  if stop == 1:
+   break
+ stop = 0
+ print("///////////////////")
+ 
+ '''coin bas droit'''
+ for i in reversed(range(len(input_image))):
+  for j in reversed(range(len(input_image[0]))):
+      if input_image[i,j] > 0:
+       CBD = i + 1, j+ 1
+       stop = 1
+       break
+  if stop == 1:
+   break
+ stop = 0
+       
+ print("///////////////////")
+ 
+ '''coin bas droit'''
+ for i in reversed(range(len(input_image))):
+  for j in range(len(input_image[0])):
+      if input_image[i,j] > 0:
+       CBG = i + 1, j+ 1 
+       stop = 1
+       break
+  if stop == 1:
+   break
+ stop = 0
+
+ print (CHG,CHD,CBG,CBD)
+ 
+ bounding_box = 0
+ if CHG[0] < CBD[0]:
+  bounding_box = str(CHG) + " " + str(CBD)
+  print(bounding_box)
+ if CHG[0] > CBD[0]: 
+  bounding_box = str(CHD) + " " + str(CBG)
+  print(bounding_box)
+  
+  '''
+      x1>x   x1 = 0
+      y<y1   y1 =y
+      x2 < x x2 = x
+      y2 < y y2=y
+  '''
+    
+ return input_image
  
   
 
 maTable = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+'''print(reverse_table(maTable))'''
 '''print(max_value(maTable))'''
 '''print(average_above_zero(maTable))'''
-print(reverse_table(maTable))
+H = 12
+W = 10
+monImage = np.zeros((H,W))
+monImage[8:10,7:9] = np.ones((2,2))
+monImage[2:4,3:5] = np.ones((2,2))*2
+
+print(roi_bbox(monImage))
+
